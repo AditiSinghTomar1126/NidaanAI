1bf4e1fb904c6f1a7cab7316199c43a13ff57849
Aditi Singh Tomar <aditisinghtomar1126@gmail.com>
2026-09-24 22:59:19 +0530

    Deployed Backend

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -30,7 +30,7 @@ function Navbar() {
   const fetchProfile = async () => {
     try {
       const response = await fetch(
-        "http://localhost:8080/api/v1/users/profile",
+        `${import.meta.env.VITE_API_URL}/api/v1/users/profile),
         {
           credentials: "include",
         }
@@ -50,7 +50,7 @@ function Navbar() {
   const logout = async () => {
     try {
       const response = await fetch(
-        "http://localhost:8080/api/v1/users/logout",
+        `${import.meta.env.VITE_API_URL}/api/v1/users/logout),
         {
           credentials: "include",
           method: "POST",
